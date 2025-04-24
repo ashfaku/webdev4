739b2633419b92e12c0169c876ec11482005e962
Ashfak Uddin <ashmanpapet@gmail.com>
2025-04-24 17:48:30 -0400

    Separated address into five different fields

--- client/src/components/views/CampusSubmission.js
+++ client/src/components/views/CampusSubmission.js
@@ -6,7 +6,13 @@ const CampusSubmission = () => {
         <form id = "campussubmit">
             <input type="text" id="name" name="name" placeholder="College Name..." required />
             <br />
-            <input type="text" id="address" name="address" placeholder="Address..." required />
+            <div>
+                <input type="text" id="buildingnum" name="buildingnum" placeholder="Building Number..." required />
+                <input type="text" id="street" name="street" placeholder="Street..." required />
+                <input type="text" id="city" name="city" placeholder="City..." required />
+                <input type="text" id="state" name="state" placeholder="State..." required />
+                <input type="text" id="zipcode" name="zipcode" placeholder="Zip Code..." required />
+            </div>
             <br />
             <input type="text" id="description" name="description" placeholder="Description..." />
             <br />
