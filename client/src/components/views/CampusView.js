eafabc0901d7601a68644ef6333920ccdea70ab7
Ashfak Uddin <ashmanpapet@gmail.com>
2025-04-24 22:50:08 -0400

    campus view page looks similarn ow

--- client/src/components/views/CampusView.js
+++ client/src/components/views/CampusView.js
@@ -5,6 +5,7 @@ The Views component is responsible for rendering web page with data provided by 
 It constructs a React component to display a single campus and its students (if any).
 ================================================== */
 import { Link } from "react-router-dom";
+import "../../App.css"
 
 // Take in props data to construct the component
 const CampusView = (props) => {
@@ -14,18 +15,32 @@ const CampusView = (props) => {
   return (
     <div>
       <h1>{campus.name}</h1>
-      <p>{campus.address}</p>
-      <p>{campus.description}</p>
-      {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <div key={student.id}>
-            <Link to={`/student/${student.id}`}>
-              <h2>{name}</h2>
-            </Link>             
-          </div>
-        );
-      })}
+      <div class = "campusviewbox">  
+        <img src="https://images.unsplash.com/photo-1503676260728-1c00da094a0b" alt="College Campus" width="50%"></img>
+
+        <p>Address: {campus.address}</p>
+        <p>Description: {campus.description}</p>
+        <div>
+          <button id = "editcampus">Edit Campus Information</button>
+          <button id = "deletecampus">Delete Campus</button>
+        </div>
+        <h2>Total Students: {campus.students.length}</h2>
+        <table id = "campusstudentlisttable">
+          <tr>
+            <th>Student Name</th>
+            <th></th>
+          </tr>
+          {campus.students.map( student => {
+            let name = student.firstname + " " + student.lastname;
+            return (
+              <tr>
+                <th class = "tablecell">{name}</th>
+                <th class = "tablecell"><button>Unenroll</button></th>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </div>
   );
 };
