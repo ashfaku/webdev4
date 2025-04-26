b74d513c9ac95d66b74baba182168bd462d695d8
Ashfak Uddin <ashmanpapet@gmail.com>
2025-04-26 11:43:35 -0400

    Home page view is setup

--- client/src/components/views/HomePageView.js
+++ client/src/components/views/HomePageView.js
@@ -4,11 +4,32 @@ HomePageView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
+import "../../App.css";
 const HomePageView = () => {
   // Render Home page view
   return (
     <div >
       <h1>Home Page</h1>
+      <div id = "homepagegrid">
+        <div className="box">
+          View Campuses
+          <div>
+            <button className="homepagebutton">Click Here!</button>
+          </div>
+          <div>
+            <img className="homepageimg" src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-4.0.3&auto=format&fit=crop&w=1400&q=80" alt="Campus" />
+          </div>
+        </div>
+        <div className="box">
+          View Students
+          <div> 
+            <button className="homepagebutton">Click Here!</button>
+          </div>
+          <div>
+            <img className="homepageimg" src="https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-4.0.3&auto=format&fit=crop&w=1400&q=80" alt = "Students" />
+          </div>
+        </div>
+      </div>
     </div>
   );    
 }
