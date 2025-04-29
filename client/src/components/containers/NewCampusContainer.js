310d0c015703f6f0a7e98bf2aaf60b77995ded8f
Ashfak Uddin <ashmanpapet@gmail.com>
2025-04-28 21:36:54 -0400

    oops. i was adding the same campus twice for new ones, my b

--- client/src/components/containers/NewCampusContainer.js
+++ client/src/components/containers/NewCampusContainer.js
@@ -9,27 +9,27 @@ import { Redirect } from 'react-router-dom';
 const NewCampusContainer = (props) => {
     const [redirect, setRedirect] = useState(false);
     const [redirectID, setRedirectID] = useState(null);
-    const uploadToDatabase = async (name, address, description) => {
-        console.log("Uploading?");
-        try {
-            const response = await fetch("http://localhost:3001/api/campuses", {
-                method: "POST",
-                headers: {
-                "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    name,
-                    address,
-                    description,
-                }),
-            });
-            const responseData = await response.json();
-            console.log("Response from backend:", responseData);
-        } 
-        catch (error) {
-            console.error("Error sending data:", error);
-        };
-    }
+    // const uploadToDatabase = async (name, address, description) => {
+    //     console.log("Uploading?");
+    //     try {
+    //         const response = await fetch("http://localhost:3001/api/campuses", {
+    //             method: "POST",
+    //             headers: {
+    //             "Content-Type": "application/json",
+    //             },
+    //             body: JSON.stringify({
+    //                 name,
+    //                 address,
+    //                 description,
+    //             }),
+    //         });
+    //         const responseData = await response.json();
+    //         console.log("Response from backend:", responseData);
+    //     } 
+    //     catch (error) {
+    //         console.error("Error sending data:", error);
+    //     };
+    // }
 
     const handleSubmit = async (event) => {
         event.preventDefault(); 
@@ -41,7 +41,7 @@ const NewCampusContainer = (props) => {
         const description = data.description;
         const name = data.name;
         console.log(name, address, description);
-        uploadToDatabase(name, address, description);
+        // uploadToDatabase(name, address, description);
         let newCampus = await props.addCampus({
             name: name,
             address: address,
